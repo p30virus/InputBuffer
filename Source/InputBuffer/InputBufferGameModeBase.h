8b5495c76b324e6bb0e966b06186921502b488a8
Gabriel <gabriel.labarrera.vega@gmail.com>
2021-10-23 12:59:28 -0500

    Init

---
+++ Source/InputBuffer/InputBufferGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InputBufferGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INPUTBUFFER_API AInputBufferGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
